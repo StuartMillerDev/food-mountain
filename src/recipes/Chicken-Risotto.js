9c299c93168e4387011d050432bbff2c054d99e3
Stuart Miller <stuartmillerdev@gmail.com>
2020-09-22 20:59:51 -0400

    Adding Chicken Risotto

---
+++ src/recipes/Chicken-Risotto.js
@@ -0,0 +1,59 @@
+module.exports = {
+    title: "Inspired Chicken Risotto",
+    slug: "inspired-chicken-risotto",
+    shortDescription: "A warm and satisfying dish that tastes as good as a loving hug feels!",
+    description: " An arborio rice dish that uses a specific technique to create a self-sauce that has a deep and rich flavor. Great for filling hungry family members!",
+    ingredients: [
+        {
+            quantity: 1,
+            units: 'Cups',
+            item: 'Aborio Rice'
+        },
+        {
+            quantity: 0.5,
+            units: 'Cups',
+            item: 'Sweet Onion'
+        },
+        {
+            quantity: 2,
+            units: 'Table Spoons',
+            item: 'Extra Virgin Olive Oil'
+        },
+        {
+            quantity: 1,
+            units: 'Whole',
+            item: 'Large Bulb Garlic',
+            mods:['Diced', 'Crushed']
+        },
+        {
+            quantity: 2,
+            units: 'Whole',
+            item: 'Chicken Thigh',
+            mods: ['Pan Seared', 'Sliced']
+        }
+    ],
+    instructions: [
+      'Prepare ingredients ahead of cooking, dice onions, crush garlic and wash/rinse chicken.',
+      'Add oilive oil to cast iron pan and heat till oil begins to vapor. Add onions and stir to sweat onions.',
+      'Add garlic to onions and add seasonings. Mix until color is well balanced. Add rice ontop of onions and mix allowing the rice to soak up all the flavors.',
+      'Add enough chicken stock to cover rice and boil.'
+    ],
+    equipment:{
+      required:[
+        {
+          name:'10 inch cast iron pan',
+          link: '',
+          plug: ""
+        }
+      ],
+      recommended:[
+        
+      ]
+    },
+    tags: [
+      "Cast-Iron",
+      "Chicken",
+      ""
+      
+    ]
+  }
